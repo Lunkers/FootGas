8569864ff7ad631bdbc655db77f2c6b6dba303c1
Fredrik Lundkvist <flundkvi@kth.se>
2022-04-15 16:31:31 +0200

    Initial Commit

---
+++ FootGas/FootGasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootGasApp.swift
+//  FootGas
+//
+//  Created by Foppe on 2022-04-15.
+//
+
+import SwiftUI
+
+@main
+struct FootGasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
